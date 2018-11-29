ed8e9673c063b798f71bc150a3e80f2193f05352
ksheedy <shee0067@algonquinlive.com>
2018-11-29 16:11:23 -0500

    Fix issue with current state in nav

--- js/main.js
+++ js/main.js
@@ -8,22 +8,3 @@ $('.nav-items a').on('click', function () {
   $('.nav-items').toggleClass('open');
   $('.nav-toggle').toggleClass('open');
 });
-
-//Current state in navigation
-$('#nav').onePageNav({
-  currentClass: 'current',
-  changeHash: false,
-  scrollSpeed: 750,
-  scrollThreshold: 0.5,
-  filter: '',
-  easing: 'swing',
-  begin: function() {
-    //I get fired when the animation is starting
-  },
-  end: function() {
-    //I get fired when the animation is ending
-  },
-  scrollChange: function($currentListItem) {
-    //I get fired when you enter a section and I pass the list item of the section
-  }
-});
